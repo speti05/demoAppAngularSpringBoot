40860fa9dc8edef92d807f2d435e27b9bc3825f2
speti05 <speti05@gmail.com>
2019-01-04 14:35:57 +0100

    move frontend to maved project (untested)

--- registration/src/main/java/com/company/registration/RegistrationApplication.java
+++ registration/src/main/java/com/company/registration/RegistrationApplication.java
@@ -1,18 +1,7 @@
 package com.company.registration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class RegistrationApplication {
@@ -20,38 +9,4 @@ public class RegistrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationApplication.class, args);
 	}
-	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/companies/**")
-                	.allowedOrigins("http://localhost:4200")
-                	.allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE");
-            }
-        };
-    }
-	
-	@Configuration
-	  @Order(SecurityProperties.BASIC_AUTH_ORDER)//ACCESS_OVERRIDE_ORDER
-	  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	    @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	      http
-	      .logout().logoutSuccessUrl("/login")
-	      .and()
-	        .authorizeRequests()
-	          .antMatchers("/index.html", "/", "/home", "/login").permitAll()
-	          .anyRequest().authenticated();
-	    }
-	  }
-	
-	  @Autowired
-	  public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-	    auth.inMemoryAuthentication()
-	      .withUser("user").password("test").roles("USER")
-	    .and()
-	      .withUser("manager").password("test").roles("USER", "ADMIN", "READER", "WRITER");
-	  }
 }
--- registration/src/main/java/com/company/registration/SecurityConfiguration.java
+++ registration/src/main/java/com/company/registration/SecurityConfiguration.java
@@ -1,28 +1,69 @@
-package com.company.registration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
-
-	  @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	      http.cors().and()
-	      .logout().logoutSuccessUrl("/login")
-	      .and()
-	        .authorizeRequests()
-	          .antMatchers("/index.html", "/", "/login").permitAll()
-	          .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
-	    }
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.inMemoryAuthentication().withUser("user").password("test").roles("USER");
-		}
-		
-}
+//package com.company.registration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//	    http
+//        .authorizeRequests().antMatchers("/login", "index.html", "/").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").isCustomLoginPage();
+//	}
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("user").password("test").roles("USER");
+//	}
+	
+	
+//OLD CONF	
+	
+//	@Bean
+//  public WebMvcConfigurer corsConfigurer() {
+//      return new WebMvcConfigurerAdapter() {
+//          @Override
+//          public void addCorsMappings(CorsRegistry registry) {
+//              registry.addMapping("/**")
+//              	
+//              	.allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE");
+//          }
+//      };
+//  }
+//	
+//	@Configuration
+//	@EnableWebSecurity
+//	  @Order(SecurityProperties.BASIC_AUTH_ORDER)//ACCESS_OVERRIDE_ORDER
+//	  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//	    @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	      http.cors().and()
+//	      .logout().logoutSuccessUrl("/login")
+//	      .and()
+//	        .authorizeRequests()
+//	          .antMatchers("/index.html", "/", /*"/home", */ "/login").permitAll()
+//	          .anyRequest().authenticated();
+//	    }
+//
+//		@Override
+//		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//			auth.inMemoryAuthentication().withUser("user").password("test").roles("USER");
+//		}
+//	  }
+	
+	
+	
+//	  @Autowired
+//	  public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+//	    auth.inMemoryAuthentication()
+//	      .withUser("user").password("test").roles("USER")
+//	    .and()
+//	      .withUser("manager").password("test").roles("USER", "ADMIN", "READER", "WRITER");
+//	  }
+//}
