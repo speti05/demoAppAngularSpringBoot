e7624c4e35902477d75e04fc4208a9c3b07f19f2
speti05 <speti05@gmail.com>
2019-01-10 16:01:18 +0100

    Fixed: spring catched alngular routes

--- registration/src/main/java/com/company/registration/config/SecurityConfiguration.java
+++ registration/src/main/java/com/company/registration/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-//package com.company.registration;
+//package com.company.registration.config;
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
---
+++ registration/src/main/java/com/company/registration/config/StateForwardConfiguration.java
@@ -0,0 +1,20 @@
+package com.company.registration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//Config to prevent string to catch angular states
+@Configuration
+public class StateForwardConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
